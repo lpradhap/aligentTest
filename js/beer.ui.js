eb63d7d242abc090b3d206c0dbeb91341d1f1b06
lpradhap <kandguy@gmail.com>
2018-03-12 11:46:21 +0530

    loading popup inclusion
    loading popup inclusion and also removing rangeslider initiation from UI file

--- js/beer.ui.js
+++ js/beer.ui.js
@@ -1,42 +1,53 @@
+
 var BeerApp = BeerApp || {};
 
-BeerApp.UI = (function(){
-    
+BeerApp.UI = (function () {
+
     const ui = {};
 
-    ui.init = function(){
-        ui.rangeSlider();
+    ui.init = function () {
+        
     }
-    
-    ui.rangeSlider = function() {
+
+    ui.rangeSlider = function () {
         //check for slider
-        if(!$('.range-slider').length) {
+        if (!$('.range-slider').length) {
             return
         }
 
-        $('.range-slider').each(function(){
+        $('.range-slider').each(function () {
             var maxRange = parseInt($(this).attr('data-max-range'))
-            var minRange= parseInt($(this).attr('data-min-range'))
+            var minRange = parseInt($(this).attr('data-min-range'))
 
             $(this).slider({
                 range: true,
                 min: minRange,
                 max: maxRange,
-                values: [minRange, maxRange ],
-                slide: function( event, ui ) {
-                  $(this).closest('.range-control').find(".min-amt").val(ui.values[ 0 ]);
-                  $(this).closest('.range-control').find(".max-amt").val(ui.values[ 1 ] );
+                values: [minRange, maxRange],
+                slide: function (event, ui) {
+                    $(this).closest('.range-control').find(".min-amt").val(ui.values[0]);
+                    $(this).closest('.range-control').find(".max-amt").val(ui.values[1]);
                 }
-              });
+            });
         })
-        
 
 
+
+    }
+
+    ui.loader = {
+        _panel: $('.loading-panel'),
+        show: function () {
+            $(this._panel).show()
+        },
+        hide: function () {
+            $(this._panel).hide()
+        }
     }
 
     return ui
 })()
 
-$(document).ready(function(){
+$(document).ready(function () {
     BeerApp.UI.init()
 })
