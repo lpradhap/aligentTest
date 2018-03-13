e728250a529a42405d0e621f5ad549d0472396e4
lpradhap <kandguy@gmail.com>
2018-03-13 16:36:29 +1030

    Mobile Responsive development
    Mobile Responsive development
    -- rwd css file is included
    -- mobMenu functions are included

--- js/beer.ui.js
+++ js/beer.ui.js
@@ -6,6 +6,9 @@ BeerApp.UI = (function () {
     const ui = {};
 
     ui.init = function () {
+        //mobile menu slider
+        ui.mobMenu.init()
+
         //initiate all UI functions
         $('body').on('click', '.product-list li', function () {
 
@@ -18,13 +21,42 @@ BeerApp.UI = (function () {
         _panel: $('.product-list .scroll-panel'),
         setHeight: function () {
             //clear set height
-            ui.productListHeight._panel.css('height','')
-            
+            ui.productListHeight._panel.css('height', '')
+
             let windowH = $(document).height();
             let headerH = $('.header-panel').innerHeight();
 
             //set new height
             ui.productListHeight._panel.height((windowH - headerH) - 5)
+        }
+    }
+
+    //mobile menu
+    ui.mobMenu = {
+        _panel: '.mobile-menu',
+        _activeClass: 'active',
+        _sliderPanel: $('.product-list'),
+        init: function () {
+            //check mobile menu button click 
+            $(document).on('click', ui.mobMenu._panel, function () {
+                ui.mobMenu.slide()
+            })
+
+        },
+        slide: function (el) {
+            //slide animation is checked with depending on a active class
+            if ($(ui.mobMenu._panel).hasClass(ui.mobMenu._activeClass)) {
+                 ui.mobMenu._sliderPanel.animate({
+                    left: '-100%'
+                }, 150)
+                $(ui.mobMenu._panel).removeClass(ui.mobMenu._activeClass)
+            } else {
+                ui.mobMenu._sliderPanel.animate({
+                    left: 0
+                }, 150)
+                $(ui.mobMenu._panel).addClass(ui.mobMenu._activeClass)
+            }
+
         }
     }
 
